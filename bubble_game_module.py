682bbcf80f500718874cbce81cd0f95bb7483ade
Tripura657 <tripurateja4545@gmail.com>
2025-04-05 17:29:50 +0530

    Update bubble_game_module.py

--- bubble_game_module.py
+++ bubble_game_module.py
@@ -45,7 +45,7 @@ def run():
             st.markdown(f'<div class="feeling-box">💭 "{feeling}"</div>', unsafe_allow_html=True)
 
             if is_negative(feeling):
-                st.image("https://cdn-icons-png.flaticon.com/512/5117/5117343.png", width=100, caption="Toilet 🚽")
+                st.image("toilet.png", width=100, caption="Toilet 🚽")
                 if st.button("🚽 Flush it away!"):
                     st.session_state["flushed"] = True
             else:
